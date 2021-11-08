59c2d8116540a316834ddb84acc11bf4468614db
abdul <abdul.tanveer@gmail.com>
2021-11-08 15:54:15 +0530

    counter increment button click

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
  
 
 function App() {
   return (
     <div className="App">
+
+    <Counter/>
+
     <Message/>
 
     <Greet name = "abdul"  college = "IIT">
