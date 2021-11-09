7fe1ecf21362a976b337e8f67254b9e70a2b76e8
abdul <abdul.tanveer@gmail.com>
2021-11-09 16:38:29 +0530

    map using a const nameslist

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['abdul','ansari','tanveer','dxc']
-    return (
-        <div>
-           {
-               names.map(name => <h2>{name}</h2>)
-           }
+    const names = ['abdul', 'ansari', 'tanveer', 'dxc','react']
+    const namesList = names.map(name => <h2>{name}</h2>)
 
-        </div>
+    return (
+        <div>{namesList}</div>
     )
 }
 
