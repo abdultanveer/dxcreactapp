58382dfa56d060062bd04fabb64206001e53100d
abdul <abdul.tanveer@gmail.com>
2021-11-12 15:16:15 +0530

    refs in class components -- focus a child from a parent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import FocusInput from './components/refs/FocusInput';
+import Input from './components/refs/Input';
 
-import RefsDemo from './components/RefsDemo';
 
 
 function App() {
   return (
     <div className="App">
-    <RefsDemo/>
+      <FocusInput/>
     </div>
   );
 }
